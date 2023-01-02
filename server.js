d0b1df521a8aa83069244d244b1d3a906f041e3d
CG-B05S <104055011+CG-B05S@users.noreply.github.com>
2023-01-02 09:37:13 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoute.js';
+import userRouter from './routes/userRoutes.js';
+import orderRouter from './routes/orderRoutes.js';
+
+dotenv.config();
+
+mongoose.set('strictQuery', false);
+
+mongoose
+  .connect("mongodb+srv://admin:Rakesh2696@cluster0.pogpo09.mongodb.net/test?retryWrites=true&w=majority")
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/api/keys/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+
+app.use('/api/send', seedRouter);
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`serve at https://localhost:${port}`);
+});
